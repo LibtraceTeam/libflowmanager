1b67337835decf2b46075e0f6575a5a042aab511
Shane Alcock <salcock@waikato.ac.nz>
2020-10-29 16:51:18 +1300

    Fix copy-paste issues with rpmpkg-build.sh script

--- rpmpkg-build.sh
+++ rpmpkg-build.sh
@@ -6,13 +6,9 @@ export QA_RPATHS=$[ 0x0001 ]
 SOURCENAME=`echo ${GITHUB_REF##*/} | cut -d '-' -f 1`
 
 ./bootstrap.sh && ./configure && make dist
-cp libtrace-*.tar.gz ~/rpmbuild/SOURCES/${SOURCENAME}.tar.gz
-cp rpm/libtrace4.spec ~/rpmbuild/SPECS/
-#cp rpm/libtrace4-dag.spec ~/rpmbuild/SPECS/
+cp libflowmanager-*.tar.gz ~/rpmbuild/SOURCES/${SOURCENAME}.tar.gz
+cp rpm/libflowmanager4.spec ~/rpmbuild/SPECS/
 
-cd ~/rpmbuild && rpmbuild -bb --define "debug_package %{nil}" SPECS/libtrace4.spec
+cd ~/rpmbuild && rpmbuild -bb --define "debug_package %{nil}" SPECS/libflowmanager4.spec
 
-#if [[ "$1" =~ centos* ]]; then
-#       cd ~/rpmbuild && rpmbuild -bb --define "debug_package %{nil}" SPECS/libtrace4-dag.spec
-#fi
 
